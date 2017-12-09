b85501ac8882d8ec074ac2b4bf0caa7daa55dbdf
nadejhdaa <design7933@ya.ru>
2017-12-09 17:40:00 +0300

    modules themes added 2

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -244,7 +244,21 @@
  *   );
  * @endcode
  */
-$databases = array();
+$databases = array (
+  'default' => 
+  array (
+    'default' => 
+    array (
+      'database' => '8express',
+      'username' => '8express',
+      'password' => 'express888',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
 
 /**
  * Access control for update.php script.
@@ -276,7 +290,7 @@ $update_free_access = FALSE;
  *   $drupal_hash_salt = file_get_contents('/home/example/salt.txt');
  *
  */
-$drupal_hash_salt = '';
+$drupal_hash_salt = 'tQu-yKYQcNB5A5Zfmny38ZWZ-ySujx97CKHiTLu3E28';
 
 /**
  * Base URL (optional).
